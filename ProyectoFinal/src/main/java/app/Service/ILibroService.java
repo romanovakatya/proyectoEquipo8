1b164aed2314e8c3b9c135b9976cc0ac9990e1d9
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-21 18:41:58 +0200

    Create ILibroService.java

---
+++ ProyectoFinal/src/main/java/app/Service/ILibroService.java
@@ -0,0 +1,19 @@
+package app.Service;
+
+import java.util.List;
+
+import app.DTO.Libro;
+
+public interface ILibroService {
+
+	public List<Libro> listarLibro(); //Listar todos los Libros
+	
+	public Libro guardarLibro(Libro libro);	//Guarda un Libro CREATE
+	
+	public Libro actualizarLibro(Libro libro); //Actualiza datos del Libro UPDATE
+	
+	public void eliminarLibro(Long id);// Elimina el Libro DELETE
+	
+	public Libro libroXID(Long id); //Leer datos de un Libro por ID READ
+	
+}
