ac8ecea40814c36a38a6e6d6f3668a410902d810
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-21 18:41:50 +0200

    Create EditorialServiceImpl.java

---
+++ ProyectoFinal/src/main/java/app/Service/EditorialServiceImpl.java
@@ -0,0 +1,47 @@
+package app.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.DAO.IEditorialDAO;
+import app.DTO.Editorial;
+
+@Service
+public class EditorialServiceImpl implements IEditorialService {
+
+	@Autowired
+	IEditorialDAO iEditorialDAO;
+	
+	@Override
+	public List<Editorial> listarEditorial() {
+		// TODO Auto-generated method stub
+		return iEditorialDAO.findAll();
+	}
+
+	@Override
+	public Editorial guardarEditorial(Editorial editorial) {
+		// TODO Auto-generated method stub
+		return iEditorialDAO.save(editorial);
+	}
+
+	@Override
+	public Editorial actualizarEditorial(Editorial editorial) {
+		// TODO Auto-generated method stub
+		return iEditorialDAO.save(editorial);
+	}
+
+	@Override
+	public void eliminarEditorial(Long id) {
+		// TODO Auto-generated method stub
+		iEditorialDAO.deleteById(id);
+	}
+
+	@Override
+	public Editorial editorialXID(Long id) {
+		// TODO Auto-generated method stub
+		return iEditorialDAO.findById(id).get();
+	}
+
+}
