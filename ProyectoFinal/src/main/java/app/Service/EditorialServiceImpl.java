acc1fe5afd04abf4c8669291fa599404cf78c6ff
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-27 09:03:35 +0200

    Update EditorialServiceImpl.java

--- ProyectoFinal/src/main/java/app/Service/EditorialServiceImpl.java
+++ ProyectoFinal/src/main/java/app/Service/EditorialServiceImpl.java
@@ -1,12 +1,12 @@
-package app.Service;
+package app.service;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import app.DAO.IEditorialDAO;
-import app.DTO.Editorial;
+import app.dao.IEditorialDAO;
+import app.dto.Editorial;
 
 @Service
 public class EditorialServiceImpl implements IEditorialService {
