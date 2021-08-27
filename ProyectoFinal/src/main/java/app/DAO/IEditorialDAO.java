2c05e1ab219c4dbef40b4dae8cd7267ec10fa6ca
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-27 09:06:23 +0200

    Delete IEditorialDAO.java

--- ProyectoFinal/src/main/java/app/DAO/IEditorialDAO.java
+++
@@ -1,9 +0,0 @@
-package app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.dto.Editorial;
-
-public interface IEditorialDAO extends JpaRepository<Editorial, Long> {
-
-}
