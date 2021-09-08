2d1f281d2f84d40105060693cd0427b4a3b8aaf5
joelGit99 <87014970+joelGit99@users.noreply.github.com>
2021-09-08 10:14:46 +0200

    Commit Libro OneToMany Ejemplar

---
+++ ProyectoFinal/src/main/java/app/dto/Libro.java
@@ -0,0 +1,90 @@
+package app.dto;
+
+import javax.persistence.*;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // busca ultimo valor e incrementa desde id final de db
+	private Long id;
+	@Column(name = "autor") // no hace falta si se llama igual
+	private String autor;
+	@Column(name = "titulo") // no hace falta si se llama igual
+	private String titulo;
+	@Column(name = "descripcion") // no hace falta si se llama igual
+	private String descripcion;
+	
+	@ManyToOne
+	@JoinColumn(name = "editorial")
+	private Editorial editorial;
+	
+	@OneToMany
+	@JoinColumn(name="ejemplares")
+	private List<Ejemplar> ejemplares;
+	
+	public Libro() {
+		
+	}
+	
+	public Libro(String autor,String titulo,String descripcion,Editorial editorial) {
+		this.autor=autor;
+		this.titulo=titulo;
+		this.descripcion=descripcion;
+		this.editorial=editorial;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Editorial getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(Editorial editorial) {
+		this.editorial = editorial;
+	}
+	
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Ejemplar")
+	public List<Ejemplar> getEjemplares() {
+		return ejemplares;
+	}
+
+	public void setEjemplares(List<Ejemplar> ejemplares) {
+		this.ejemplares = ejemplares;
+	}
+
+	@Override
+	public String toString() {
+		return "Libro [id=" + id + ", autor=" + autor + ", titulo=" + titulo + ", descripcion=" + descripcion
+				+ ", editorial=" + editorial + "]";
+	}
+	
+	
+	
+}
