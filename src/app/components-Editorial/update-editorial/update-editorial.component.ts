e8b61f439004d69b6d591f5a03314444b7f08d57
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-10 09:07:15 +0200

    Create update-editorial.component.ts

---
+++ src/app/components-Editorial/update-editorial/update-editorial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-editorial',
+  templateUrl: './update-editorial.component.html',
+  styleUrls: ['./update-editorial.component.css']
+})
+export class UpdateEditorialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
