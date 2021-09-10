7c9dc9b2fc771dba2b0f58bf1d200a5abb798ba4
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-10 13:14:26 +0200

    Update update-editorial.component.ts

--- src/app/components-Editorial/update-editorial/update-editorial.component.ts
+++ src/app/components-Editorial/update-editorial/update-editorial.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-update-editorial',
   templateUrl: './update-editorial.component.html',
-  styleUrls: ['./update-editorial.component.css']
+  styleUrls: ['./update-editorial.component.scss']
 })
 export class UpdateEditorialComponent implements OnInit {
 
