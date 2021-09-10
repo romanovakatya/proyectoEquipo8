dbc412603b366249121eb728a3687d567e8b6903
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-10 09:05:53 +0200

    Create mostrar-editorial.component.ts

---
+++ src/app/components-Editorial/mostrar-editorial/mostrar-editorial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-editorial',
+  templateUrl: './mostrar-editorial.component.html',
+  styleUrls: ['./mostrar-editorial.component.css']
+})
+export class MostrarEditorialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
