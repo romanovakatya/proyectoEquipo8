bc257a297650744145dc990b4f7d5e83dce7ae30
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-10 13:14:17 +0200

    Update principal-editorial.component.ts

--- src/app/components-Editorial/principal-editorial/principal-editorial.component.ts
+++ src/app/components-Editorial/principal-editorial/principal-editorial.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-principal-editorial',
   templateUrl: './principal-editorial.component.html',
-  styleUrls: ['./principal-editorial.component.css']
+  styleUrls: ['./principal-editorial.component.scss']
 })
 export class PrincipalEditorialComponent implements OnInit {
 
