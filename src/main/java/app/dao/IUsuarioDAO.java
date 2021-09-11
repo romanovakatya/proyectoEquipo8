cbb13498ce87993e37b464308f9810a61c2685ec
Katia <katerina.bogrom@gmail.com>
2021-09-11 09:48:51 +0200

    cambio de raiz

---
+++ src/main/java/app/dao/IUsuarioDAO.java
@@ -0,0 +1,11 @@
+package app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import app.dto.Usuario;
+
+public interface IUsuarioDAO extends JpaRepository<Usuario, Long> {
+
+	public Usuario findByUsername(String username);
+	
+}
---
+++ src/main/java/app/dto/Usuario.java
@@ -0,0 +1,152 @@
+package app.dto;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+	
+	// Atributos de la entidad usuarios
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="contrasena")
+	private String contrasena;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="apellidos")
+	private String apellidos;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="comments")
+	private String comments;
+	
+	@OneToMany
+	@JoinColumn(name="id")
+	private List<EjemplarPresta> ejemplarPrestado;
+	@OneToMany
+	@JoinColumn(name="id")
+	private List<Ejemplar> ejemplar;
+	
+	// Constructores
+	public Usuario() {
+
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @param contrasena
+	 * @param nombre
+	 * @param apellidos
+	 * @param username
+	 * @param email
+	 * @param @param email
+	 */
+	public Usuario(Long id, String contrasena, String nombre, String apellidos, String username, String email, String comments) {
+		super();
+		this.id = id;
+		this.contrasena = contrasena;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.username = username;
+		this.email = email;
+		this.comments = comments;
+	}
+	
+	// Getters y setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "EjemplarPrestado")
+	public List<EjemplarPresta> getEjemplarPrestado() {
+		return ejemplarPrestado;
+	}
+
+	public void setEjemplarPrestado(List<EjemplarPresta> ejemplarPrestado) {
+		this.ejemplarPrestado = ejemplarPrestado;
+	}
+	
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Ejemplar")
+	public List<Ejemplar> getEjemplar() {
+		return ejemplar;
+	}
+
+	public void setEjemplar(List<Ejemplar> ejemplar) {
+		this.ejemplar = ejemplar;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", contrasena=" + contrasena + ", nombre=" + nombre + ", apellidos=" + apellidos
+				+ ", username=" + username + ", email=" + email + "]";
+	}
+	
+}
