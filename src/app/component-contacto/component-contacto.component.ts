9c41321ed79bcca78f35920bc53de048a27a07dd
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-11 12:34:33 +0200

    Create component-contacto.component.ts

---
+++ src/app/component-contacto/component-contacto.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-contacto',
+  templateUrl: './component-contacto.component.html',
+  styleUrls: ['./component-contacto.component.scss']
+})
+export class ComponentContactoComponent implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+}
