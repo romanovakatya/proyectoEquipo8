906ff979aac7678838f0c663a5def263519eeb80
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-14 11:52:17 +0200

    Create usuario.service.ts

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
+
+const baseUrl = 'https://libro-systems.herokuapp.com/api/usuarios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  get(id: any): Observable<Usuario> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+}
