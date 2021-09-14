a7b7d4c2c0679b30cffe61a252c4230eba80afd5
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-14 11:52:12 +0200

    Update libro.service.ts

--- src/app/services/libro.service.ts
+++ src/app/services/libro.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Libro } from '../models/libro.model';
+import { Editorial } from '../models/editorial.model';
+
+const baseUrl = 'https://libro-systems.herokuapp.com/api/libros';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibroService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Libro[]> {
+    return this.http.get<Libro[]>(baseUrl);
+  }
+
+  getID(id: any): Observable<Editorial> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  getAutor(autor: any): Observable<Editorial> {
+    return this.http.get(`${baseUrl}/autor/${autor}`);
+  }
+
+  getTitulo(titulo: any): Observable<Editorial> {
+    return this.http.get(`${baseUrl}/titulo/${titulo}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
 }
