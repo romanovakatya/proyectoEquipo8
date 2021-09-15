d1888646ddf9fa9ca648f64c34c954853665b938
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-15 17:50:51 +0200

    Update mostrar-editorial.component.ts

--- src/app/components-Editorial/mostrar-editorial/mostrar-editorial.component.ts
+++ src/app/components-Editorial/mostrar-editorial/mostrar-editorial.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EditorialService } from 'src/app/services/editorial.service';
 
 @Component({
   selector: 'app-mostrar-editorial',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MostrarEditorialComponent implements OnInit {
 
-  constructor() { }
+  id:any=0;
+
+  editorial:any=null;
+
+  constructor(private editorialService: EditorialService,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getEditorial();
+  }
+
+  getEditorial(){
+    this.id=this.route.snapshot.paramMap.get("id");
+
+    this.editorialService.getID(this.id)
+    .subscribe(
+      result =>{
+        this.editorial = result;
+        console.log(this.editorial);
+      },
+      error =>{
+        console.log("Problemas");
+      }
+      );
   }
 
 }
