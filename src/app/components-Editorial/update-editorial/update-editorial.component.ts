1ae2d99f5e4debaf6a02d5dd7db345bf2fb699b0
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-15 17:51:09 +0200

    Update update-editorial.component.ts

--- src/app/components-Editorial/update-editorial/update-editorial.component.ts
+++ src/app/components-Editorial/update-editorial/update-editorial.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Editorial } from 'src/app/models/editorial.model';
+import { EditorialService } from 'src/app/services/editorial.service';
 
 @Component({
   selector: 'app-update-editorial',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateEditorialComponent implements OnInit {
 
-  constructor() { }
+  currentEditorial: Editorial = {
+    nombre: '',
+    direccion: '',
+    pagina_web:''
+  }
+
+  id:any=0;
+
+  editorial:any=null;
+
+  constructor(private editorialService: EditorialService,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getEditorial();
   }
+
+  getEditorial(){
+    this.id=this.route.snapshot.paramMap.get("id");
+
+    this.editorialService.getID(this.id)
+    .subscribe(
+      result =>{
+        this.editorial = result;
+        console.log(this.editorial);
+      },
+      error =>{
+        console.log("Problemas");
+      }
+      );
+  }
+
+  updateEditorial(){
+    this.id=this.route.snapshot.paramMap.get("id");
+
+    this.editorialService.update(this.id,this.currentEditorial)
+    .subscribe(
+      response => {
+        console.log(response);
+        //this.message = response.message ? response.message : 'This tutorial was updated successfully!';
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
 
 }
