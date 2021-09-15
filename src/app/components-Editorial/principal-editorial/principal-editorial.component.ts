75f9ffb8ae951f3368dae338178c506d266485fb
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-15 17:51:04 +0200

    Update principal-editorial.component.ts

--- src/app/components-Editorial/principal-editorial/principal-editorial.component.ts
+++ src/app/components-Editorial/principal-editorial/principal-editorial.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Editorial } from 'src/app/models/editorial.model';
+import { EditorialService } from 'src/app/services/editorial.service';
 
 @Component({
   selector: 'app-principal-editorial',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalEditorialComponent implements OnInit {
 
-  constructor() { }
+  editoriales:any = null;
+
+  nombre:string="";
+
+  editorial:any = null;
+
+  constructor(private editorialService: EditorialService) { }
 
   ngOnInit(): void {
+    this.getEditoriales();
+  }
+
+  getEditoriales(){
+    this.editorialService.getAll()
+    .subscribe(
+      result =>{
+        this.editoriales = result;
+        console.log(this.editoriales);
+      },
+      error =>{
+        console.log("Problemas");
+      }
+      );
+  }
+
+  getEditorialByName(){
+    this.editorialService.getNombre(this.nombre)
+    .subscribe(
+      result =>{
+        this.editoriales = result;
+        this.editorial = Object.keys(this.editoriales).map((key) => ({type: key, value: this.editoriales[key]}));
+        this.editoriales =[{id:this.editorial[0].value,nombre:this.editorial[1].value}];
+        console.log(this.editoriales);
+      },
+      error =>{
+        console.log("Problemas");
+      }
+      );
   }
 
 }
