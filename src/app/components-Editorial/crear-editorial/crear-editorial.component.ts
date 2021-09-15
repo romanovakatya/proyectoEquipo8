5e40d872e08fc8ee221726ff97794a2003576ca5
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-09-15 22:31:07 +0200

    Update crear-editorial.component.ts

--- src/app/components-Editorial/crear-editorial/crear-editorial.component.ts
+++ src/app/components-Editorial/crear-editorial/crear-editorial.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Editorial } from 'src/app/models/editorial.model';
+import { EditorialService } from 'src/app/services/editorial.service';
 
 @Component({
   selector: 'app-crear-editorial',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearEditorialComponent implements OnInit {
 
-  constructor() { }
+  currentEditorial: Editorial = {
+    nombre: '',
+    direccion: '',
+    pagina_web:''
+  }
+
+  submitted = false;
+
+  constructor(private editorialService: EditorialService) { }
 
   ngOnInit(): void {
+
+  }
+
+  crearEditorial(){
+    const data = {
+      nombre: this.currentEditorial.nombre,
+      direccion: this.currentEditorial.direccion,
+      pagina_web: this.currentEditorial.pagina_web
+    };
+
+    this.editorialService.create(data)
+    .subscribe(
+      response => {
+        console.log(response);
+        this.submitted = true;
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+  nuevaEditorial(){
+    this.submitted = false;
+    this.currentEditorial = {
+      nombre: '',
+      direccion: '',
+      pagina_web: ''
+    };
   }
 
 }
